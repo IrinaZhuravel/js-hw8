955bafbed371503084aa755b7e56557eb4258732
IrinaZhuravel <irenasuarchive@gmail.com>
2025-03-29 15:18:36 +0200

    simpleLightBox gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+
+
+const renderGallery = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}" loading="lazy"/></a></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", renderGallery);
+
+new SimpleLightbox('.gallery a', { 
+   captionsData: "alt",
+   captionDelay: 250,
+});
